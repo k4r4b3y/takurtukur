678f223b583a01b77111db849cabbc70df4b0cbf
k4r4b3y <user@host.localdomain>
2025-01-06 13:20:11 +0000

    Fix git command's env vars
    GIT_COMMITTER by itself is not a valid environment variable for `git'.
    You have to add _NAME and _EMAIL to its end and give it its values.

--- tekuti/git.scm
+++ tekuti/git.scm
@@ -212,10 +212,12 @@
                 (if parent (list "-p" parent) '()))
          #:input message
          #:env (if timestamp
-                   (list "GIT_COMMITTER=tekuti"
+                   (list "GIT_COMMITTER_NAME=tekuti"
+                         "GIT_COMMITTER_EMAIL=tekuti@example.com"
                          (format #f "GIT_COMMITTER_DATE=~a +0100" timestamp)
                          (format #f "GIT_AUTHOR_DATE=~a +0100" timestamp))
-                   (list "GIT_COMMITTER=tekuti")))))
+                   (list "GIT_COMMITTER_NAME=tekuti"
+                         "GIT_COMMITTER_EMAIL=tekuti@example.com")))))
 
 ;;;
 ;;; utilities
